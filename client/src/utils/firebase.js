40ae88f062114b4148ce0874ede68cb927996d05
Nick Cheng <nicholascheng25@gmail.com>
2021-07-05 22:44:55 +1000

    cleaning up tokens'

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,17 +1,17 @@
 import firebase from "firebase";
 import "firebase/firestore";
 import "firebase/auth";
-require('dotenv').config()
 
 
 
 const app = firebase.initializeApp({
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    projectId: PROJECT_ID,
-    storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGING_SENDER_ID,
-    appId: APP_ID,
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: "G-H2JW2PCJCM"
   })
 
   export const auth = app.auth()
