e96de8c67ddf50d9c66195d2b7a14da959c2c7fa
Nick Cheng <nicholascheng25@gmail.com>
2021-07-05 22:34:39 +1000

    clean up

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,17 +1,17 @@
 import firebase from "firebase";
 import "firebase/firestore";
 import "firebase/auth";
+require('dotenv').config()
 
 
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyDS9kqk5L7_-8e543PfSrsgQLkSC7p7ULw",
-    authDomain: "creditcards-62ddf.firebaseapp.com",
-    projectId: "creditcards-62ddf",
-    storageBucket: "creditcards-62ddf.appspot.com",
-    messagingSenderId: "301757566814",
-    appId: "1:301757566814:web:5ffb62f02efe9ffa9049cf",
-    measurementId: "G-H2JW2PCJCM"
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID,
   })
 
   export const auth = app.auth()
